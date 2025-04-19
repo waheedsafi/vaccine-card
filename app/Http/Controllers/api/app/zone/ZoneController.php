3b21c0e4af47507f8f87246d60aeadc6c4ab3aa8
waheedsafi <wahidsafi.198@gmail.com>
2025-04-19 16:48:35 +0430

    zone store list

--- app/Http/Controllers/api/app/zone/ZoneController.php
+++ app/Http/Controllers/api/app/zone/ZoneController.php
@@ -2,10 +2,50 @@
 
 namespace App\Http\Controllers\api\app\zone;
 
-use App\Http\Controllers\Controller;
+use App\Models\Zone;
+use App\Models\ZoneTrans;
+use App\Enums\LanguageEnum;
+use App\Models\AddressTran;
+use GuzzleHttp\Psr7\Message;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class ZoneController extends Controller
 {
     //
+
+
+    public function zones()
+    {
+
+        $locale = App()->getLocale();
+        $zones =    ZoneTrans::select('value as name', 'zone_id as id')->where('language_name', $locale)->get();
+
+
+        return response()->json([
+            "zone" => $zones
+        ], 200, [], JSON_UNESCAPED_UNICODE);
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData =  $request->validate([
+            'name_english' => 'required',
+            'name_pashto' => 'required',
+            "name_farsi" => 'required'
+        ]);
+        $zone =    Zone::create();
+
+        foreach (LanguageEnum::LANGUAGES as $code => $name) {
+            ZoneTrans::create([
+                'zone_id' => $zone->id,
+                'value' => $validatedData["name_{$name}"],
+                'language_name' =>  $code,
+            ]);
+        }
+
+        return response()->json([
+            "message" => __('app_translation.success')
+        ], 200, [], JSON_UNESCAPED_UNICODE);
+    }
 }
