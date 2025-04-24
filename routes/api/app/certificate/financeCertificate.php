5e3496dbdfa42abdb9de2707bebf903ab8f93d91
waheedsafi <wahidsafi.198@gmail.com>
2025-04-24 07:51:44 +0430

    finacne pyment ifn

--- routes/api/app/certificate/financeCertificate.php
+++ routes/api/app/certificate/financeCertificate.php
@@ -11,7 +11,12 @@ use App\Http\Controllers\api\app\certificate\finance\CertificatePaymentControlle
 
 
 
+
+
 Route::prefix('v1')->middleware(["authorized:" . 'finance:api'])->group(function () {
     // Route::post('/epi/certificate/detail/store', [CertificatePaymentController::class, 'searchCertificate'])->middleware(["financeHasMainPermission:" . PermissionEnum::certificate_payment->value . ',' . 'view']);
     Route::get('/finance/certificate/search', [CertificatePaymentController::class, 'searchCertificate'])->middleware(["financeHasMainPermission:" . PermissionEnum::certificate_payment->value . ',' . 'view']);
+    Route::get('/finance/certificate/info', [CertificatePaymentController::class, 'infoCertificate'])->middleware(["financeHasMainPermission:" . PermissionEnum::certificate_payment->value . ',' . 'view']);
+
+    Route::get('/finance/certificate/amount', [CertificatePaymentController::class, 'certificatePaymentAmount']);
 });
