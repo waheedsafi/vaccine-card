47520ee14afd3304a3679e153d65ff524ce9380a
sayednaweedsayedy <sayednaweedsayedy@gmail.com>
2025-04-24 07:52:48 +0430

    new task update

--- routes/api/app/certificate/epiCertificate.php
+++ routes/api/app/certificate/epiCertificate.php
@@ -11,7 +11,9 @@ Route::get('/cert', [CertificateController::class, 'generateCertificate']);
 Route::get('/activity/{id}', [CertificateController::class, 'activity']);
 
 Route::prefix('v1')->middleware(["authorized:" . 'epi:api'])->group(function () {
-    Route::get('/epi/person/information/{id}', [CertificateController::class, "personalInformation"])->middleware(["epiHasSubPermission:" . PermissionEnum::vaccine_certificate->value . "," . SubPermissionEnum::vaccine_certificate_person_info->value . ',' . 'view']);
+    Route::post('/epi/user/update/information', [CertificateController::class, 'updateInformation'])->middleware(['checkEpiAccess', "epiHasSubPermission:" . PermissionEnum::users->value . "," . SubPermissionEnum::user_information->value . ',' . 'edit']);
+    Route::get('/epi/person/information/{id}', [CertificateController::class, "updatePeopleInformation"])->middleware(["epiHasSubPermission:" . PermissionEnum::vaccine_certificate->value . "," . SubPermissionEnum::vaccine_certificate_person_info->value . ',' . 'edit']);
     Route::post('/epi/certificate/detail/store', [CertificateController::class, 'storeCertificateDetail'])->middleware(["epiHasMainPermission:" . PermissionEnum::vaccine_certificate->value . ',' . 'add']);
     Route::get('/epi/certificate/search', [CertificateController::class, 'searchCertificate'])->middleware(["epiHasMainPermission:" . PermissionEnum::vaccine_certificate->value . ',' . 'view']);
+    Route::get('/epi/person/vaccines', [CertificateController::class, 'personalVaccines'])->middleware(["epiHasSubPermission:" . PermissionEnum::vaccine_certificate->value . "," . SubPermissionEnum::vaccine_certificate_vaccination_info->value . ',' . 'view']);
 });
--- routes/api/app/users/epiUser.php
+++ routes/api/app/users/epiUser.php
@@ -13,7 +13,7 @@ Route::prefix('v1')->middleware(["authorized:" . 'epi:api'])->group(function () 
     Route::get('/epi/user/{id}', [EpiUserController::class, "user"])->middleware(['checkEpiAccess', "epiHasSubPermission:" . PermissionEnum::users->value . "," . SubPermissionEnum::user_information->value . ',' . 'view']);
     Route::delete('/epi/user/delete/profile-picture/{id}', [EpiUserController::class, 'deleteProfilePicture'])->middleware(['checkEpiAccess', "epiHasMainPermission:" . PermissionEnum::users->value . ',' . 'delete']);
     Route::post('/epi/user/update/profile-picture', [EpiUserController::class, 'updateProfilePicture'])->middleware(['checkEpiAccess', "epiHasMainPermission:" . PermissionEnum::users->value . ',' . 'edit']);
-    Route::post('/epi/user/update/information', [EpiUserController::class, 'updateInformation'])->middleware(['checkEpiAccess', "epiHasSubPermission:" . PermissionEnum::users->value . "," . SubPermissionEnum::user_information->value . ',' . 'edit']);
+    Route::post('/epi/user/update/information', [EpiUserController::class, 'updatePersonalInformation'])->middleware(['checkEpiAccess', "epiHasSubPermission:" . PermissionEnum::users->value . "," . SubPermissionEnum::user_information->value . ',' . 'edit']);
     Route::post('/epi/user/store', [EpiUserController::class, 'store'])->middleware(["epiHasMainPermission:" . PermissionEnum::users->value . ',' . 'add']);
     Route::post('/epi/user/change/account/password', [EpiUserController::class, 'changePassword'])->middleware(['checkEpiAccess', "epiHasSubPermission:" . PermissionEnum::users->value . "," . SubPermissionEnum::user_password->value . ',' . 'edit']);
 });
